7bf688645f5d06fa6e982075dce75a09574076e4
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-12 10:51:51 -0300

    feat: Cria model da tabela Categories

---
+++ models/category.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const categories = sequelize.define('Categories', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+  },
+  { timestamps: false });
+
+  categories.associate = (model) => {
+    categories.hasMany(model.PostsCategories, { foreignKey: 'id', as: 'posts' });
+  };
+
+  return categories;
+};
