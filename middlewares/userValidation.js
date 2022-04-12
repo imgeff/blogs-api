4683efd62a85d553994606bd26a58896d8392537
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-12 18:08:32 -0300

    fix: Validação de password

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -4,20 +4,20 @@ const { getByEmail } = require('../services/user');
 const userSchema = JOI.object({
   displayName: JOI.string().min(8).required(),
   email: JOI.string().email().required(),
-  password: JOI.string().min(6).max(6).required(),
+  password: JOI.string().length(6).required(),
 });
 
 const userValidation = async (req, res, next) => {
-  const { displayName, email, password, image } = req.body;
-  const dataUser = { displayName, email, password, image };
-  const userExist = await getByEmail(email);
-
-  if (userExist !== null) {
-    return res.status(409).json({ message: 'User already registered' });
-  }
+  const { displayName, email, password } = req.body;
+  const dataUser = { displayName, email, password };
 
   const { error } = userSchema.validate(dataUser);
   if (error) return res.status(400).json({ message: error.message });
+
+  const userExist = await getByEmail(email);
+  if (userExist !== null) {
+    return res.status(409).json({ message: 'User already registered' });
+  }
 
   return next();
 };
