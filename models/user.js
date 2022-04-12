33534d548d85463aa37c4557247a5db9a551d04d
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-12 10:44:59 -0300

    feat: Cria model da tabela Users

---
+++ models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('Users', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    image: { type: DataTypes.STRING },
+  },
+  { timestamps: false });
+
+  user.associate = (model) => {
+    user.hasMany(model.BlogPosts, { foreignKey: 'id', as: 'posts' });
+  };
+
+  return user;
+};
