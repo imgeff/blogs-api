f3f44532f8712cdea0ce302eac5febee09bf5ade
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-12 11:08:47 -0300

    feat: Cria model da tabela BlogPosts

---
+++ models/blogPost.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const blogPost = sequelize.define('BlogPosts', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.STRING, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
+    published: { type: DataTypes.NOW, allowNull: false },
+    updated: { type: DataTypes.NOW, allowNull: false },
+  },
+  { timestamps: true, createdAt: 'published', updatedAt: 'update' });
+
+  blogPost.associate = (model) => {
+    blogPost.belongsTo(model.Users, { foreignKey: 'userId', as: 'users' });
+  };
+
+  return blogPost;
+};
