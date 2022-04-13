cc26dfdde913b42321ae0df9ddea051701622727
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-13 11:56:53 -0300

    rename: user to userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register } = require('../controllers/user');
+const { register } = require('../controllers/userController');
 const userValidation = require('../middlewares/userValidation');
 const generateToken = require('../middlewares/generateToken');
 
