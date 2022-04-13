0123044a109c71cf3295910bd9ef1200466ff405
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-13 01:00:25 -0300

    feat: Adiciona middleware de gerar token JWT na rota POST /user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { register } = require('../controllers/user');
 const userValidation = require('../middlewares/userValidation');
+const generateToken = require('../middlewares/generateToken');
 
 const router = express.Router();
 
-router.post('/', userValidation, register);
+router.post('/', userValidation, register, generateToken);
 
 module.exports = router;
