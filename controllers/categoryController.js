a14f8584dca9c3cc4fa03d55acc097749d656a1e
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-14 14:55:53 -0300

    fix: mensagem de erro caso name não vim no body

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,7 +2,7 @@ const postService = require('../services/categoryService');
 
 const create = async (req, res) => {
   const { name } = req.body;
-  if (!name) return res.status(400).json({ message: 'name is required' });
+  if (!name) return res.status(400).json({ message: '"name" is required' });
   
   const { code, content } = await postService.create({ name });
   return res.status(code).json(content);
