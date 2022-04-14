736f88a99b9e383b967232f1e865aeac23c4ceca
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-13 23:25:28 -0300

    refactor: responde com status 401 e mensagem de erro em authToken

--- middlewares/authToken.js
+++ middlewares/authToken.js
@@ -15,7 +15,7 @@ const authToken = async (req, res, next) => {
     return next();
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: error.message });
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
 
