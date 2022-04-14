3d9e872980e5e0f2ece341ca3c606bce1ad1c0dd
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-13 23:10:07 -0300

    feat: Cria middleware de autenticação de token

---
+++ middlewares/authToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const jwtConfig = {
+  algorithm: 'HS256',
+};
+
+const secret = process.env.SECRET || 'BlogsAPI*';
+
+const authToken = async (req, res, next) => {
+  try {
+    const { authorization: token } = req.headers;
+    if (!token) return res.status(401).json({ message: 'Token not found' });
+    jwt.verify(token, secret, jwtConfig);
+
+    return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = authToken;
