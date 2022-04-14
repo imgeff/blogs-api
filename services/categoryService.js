0af49f2c313c5899d1b8b8c154269bb933424be3
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-14 14:56:52 -0300

    fix: erro ao chamar model pelo nome errado

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,7 +1,7 @@
 const model = require('../models');
 
 const create = async (category) => {
-  const { dataValues } = await model.Category.create(category);
+  const { dataValues } = await model.Categories.create(category);
   return { code: 201, content: dataValues };
 };
 
