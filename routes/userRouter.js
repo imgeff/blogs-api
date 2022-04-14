421708f307dee955969bca69bfcdd3eecda1bad4
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-14 00:09:06 -0300

    feat: Cria  rota GET /user/:id

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, getAll } = require('../controllers/userController');
+const { register, getAll, getById } = require('../controllers/userController');
 const userValidation = require('../middlewares/userValidation');
 const generateToken = require('../middlewares/generateToken');
 const authToken = require('../middlewares/authToken');
@@ -7,6 +7,8 @@ const authToken = require('../middlewares/authToken');
 const router = express.Router();
 
 router.get('/', authToken, getAll);
+
+router.get('/:id', authToken, getById);
 
 router.post('/', userValidation, register, generateToken);
 
