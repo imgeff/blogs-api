0c65aef7db932c1bb7c65a746dda4f1bf9e8dbc7
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-14 15:07:04 -0300

    feat: Cria middleware que devolve todas as categorias

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,13 +1,19 @@
-const postService = require('../services/categoryService');
+const categoryService = require('../services/categoryService');
 
 const create = async (req, res) => {
   const { name } = req.body;
   if (!name) return res.status(400).json({ message: '"name" is required' });
   
-  const { code, content } = await postService.create({ name });
+  const { code, content } = await categoryService.create({ name });
+  return res.status(code).json(content);
+};
+
+const getAll = async (_req, res) => {
+  const { code, content } = await categoryService.getAll();
   return res.status(code).json(content);
 };
 
 module.exports = {
   create,
+  getAll,
 };
