079bd2769f249976aeb862dd18f0eb3157689143
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-14 14:32:33 -0300

    refactor: Adiciona validação do campo name no controller post em create

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,6 +2,8 @@ const postService = require('../services/postService');
 
 const create = async (req, res) => {
   const { name } = req.body;
+  if (!name) return res.status(400).json({ message: 'name is required' });
+  
   const { code, content } = await postService.create({ name });
   return res.status(code).json(content);
 };
