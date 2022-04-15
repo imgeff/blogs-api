b3add5c987f24e4c7824f273c9656ac7fc7e3110
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-14 22:29:22 -0300

    feat: Adiciona middleware de validação de post na rota POST /post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { create } = require('../controllers/postController');
 const authToken = require('../middlewares/token/authToken');
+const postValidation = require('../middlewares/validation/postValidation');
 
 const router = express.Router();
 
-router.post('/', authToken, create);
+router.post('/', authToken, postValidation, create);
 
 module.exports = router;
