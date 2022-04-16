0200016f8c2a4a2c1ec566632d59f617dc2182bd
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-16 18:07:09 -0300

    feat: Cria rota DELETE /user/me

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, getAll, getById } = require('../controllers/userController');
+const { register, getAll, getById, destroy } = require('../controllers/userController');
 const createValidation = require('../middlewares/validation/createUser');
 const generateToken = require('../middlewares/token/generateToken');
 const authToken = require('../middlewares/token/authToken');
@@ -11,5 +11,7 @@ router.get('/', authToken, getAll);
 router.get('/:id', authToken, getById);
 
 router.post('/', createValidation, register, generateToken);
+
+router.delete('/me', authToken, destroy);
 
 module.exports = router;
