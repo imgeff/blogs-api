f6fdbab7dea6aabdc08171b54678630fdc7793b3
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-16 02:00:19 -0300

    feat: Cria middleware de validação de update dos posts

---
+++ middlewares/validation/updatePost.js
@@ -0,0 +1,32 @@
+const JOI = require('joi');
+const postService = require('../../services/postService');
+
+const postSchema = JOI.object({
+  title: JOI.string().required(),
+  content: JOI.string().required(),
+});
+
+const userIsAdmin = async (userId, postId) => {
+  const { content: { dataValues } } = await postService.getById(postId);
+  const checkUser = dataValues.userId === userId;
+  return checkUser;
+};
+
+const updateValidation = async (req, res, next) => {
+  const { userId } = req;
+  const { id } = req.params;
+  const { title, content, categoryIds } = req.body;
+  const dataPost = { title, content };
+
+  const { error } = postSchema.validate(dataPost);
+  if (error) return res.status(400).json({ message: error.message });
+
+  if (categoryIds) return res.status(400).json({ message: 'Categories cannot be edited' });
+  
+  const checkUser = await userIsAdmin(userId, id);
+  if (!checkUser) return res.status(401).json({ message: 'Unauthorized user' });
+  
+  return next();
+};
+
+module.exports = updateValidation;
